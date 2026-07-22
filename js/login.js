c97f546ca604825a30a64435f51e74d75cda0e3a
upheadlinenews <smartmadhavendra@gmail.com>
2026-07-22 12:04:33 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,47 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
+import {
+
+getAuth,
+signInWithEmailAndPassword,
+onAuthStateChanged
+
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import { firebaseConfig } from "./firebase-config.js";
+
+const app=initializeApp(firebaseConfig);
+
+const auth=getAuth(app);
+
+window.login=function(){
+
+const email=document.getElementById("email").value;
+
+const password=document.getElementById("password").value;
+
+signInWithEmailAndPassword(auth,email,password)
+
+.then(()=>{
+
+location.href="admin.html";
+
+})
+
+.catch(()=>{
+
+document.getElementById("msg").innerHTML="Invalid Email or Password";
+
+});
+
+}
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+console.log("Logged In");
+
+}
+
+});
